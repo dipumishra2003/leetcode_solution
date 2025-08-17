3746659633ded5aaabed68123b70e1e390a4c010
Dipu Mishra <122982296+dipumishra2003@users.noreply.github.com>
2025-08-17 20:14:52 +0530

    Time: 3 ms (27.85%), Space: 12.2 MB (10.15%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-
-    void per(vector<int>& nums, vector<vector<int>>& res, vector<int> temp, vector<bool>& visited){
-        if(temp.size()==nums.size()){
+    void per(vector<int>& nums, vector<vector<int>>& res, vector<int> temp,
+             vector<bool>& visited) {
+        if (temp.size() == nums.size()) {
             res.push_back(temp);
             return;
         }
 
-        for(int i=0; i<nums.size(); i++){
-            if(visited[i] == 0){
-                visited[i]=1;
+        for (int i = 0; i < nums.size(); i++) {
+            if (visited[i] == 0) {
+                visited[i] = 1;
                 temp.push_back(nums[i]);
-                per(nums,res,temp,visited);
-                visited[i]=0;
+                per(nums, res, temp, visited);
+                visited[i] = 0;
                 temp.pop_back();
             }
         }
     }
-    
+
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> res;
         vector<int> temp;
-        vector<bool> visited(nums.size(),0);
+        vector<bool> visited(nums.size(), 0);
         per(nums, res, temp, visited);
         return res;
     }

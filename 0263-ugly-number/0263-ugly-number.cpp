62d631a92292e8a01096a916239c376bf8f9293d
Dipu Mishra <122982296+dipumishra2003@users.noreply.github.com>
2025-10-02 18:45:28 +0530

    Time: 0 ms (100%), Space: 7.9 MB (11.19%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        if(n<=0) return false;
+        if(n==1) return true;
+        while(n>1){
+            if(n%2==0){
+                n=n/2;
+                continue;
+            }
+
+            if(n%3==0){
+                n=n/3;
+                continue;
+            }
+
+            if(n%5==0){
+                n=n/5;
+                continue;
+            }
+            if(n%2!=0 && n%3!=0 && n%5!=0) return false;
+        }
+        return true;
+    }
+};

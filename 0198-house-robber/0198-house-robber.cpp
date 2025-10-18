5f2e5cc670988f2f2df9c99e6b152d089d183684
Dipu Mishra <122982296+dipumishra2003@users.noreply.github.com>
2025-10-19 00:55:38 +0530

    Time: 0 ms (100%), Space: 10.6 MB (64.24%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxLoot(vector<int>& nums, int idx, vector<int>& dp){
+        if(idx>=nums.size()) return 0;
+        if(dp[idx] != -1) return dp[idx];
+        int steal = nums[idx] + maxLoot(nums,idx+2,dp);
+        int skip = maxLoot(nums,idx+1,dp);
+        return dp[idx] = max(steal,skip);
+    }
+    int rob(vector<int>& nums) {
+        vector<int> dp(nums.size(),-1);
+        return maxLoot(nums,0,dp);
+    }
+};
